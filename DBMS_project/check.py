79337c4f28de6920abecd14a3cb7cb4902392e6d
APPLE <apple@192.168.1.8>
2023-04-08 19:38:32 +0530

    ready

--- DBMS_project/check.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-
-cnx = mysql.connector.connect(user='root', password='rootPass',
-                              host='localhost',
-                              database='bdms')
-
-print('so far so good')
